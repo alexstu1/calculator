8535ff3cc132a940eaac6bcd8e711ba64fd6926c
alexstu1 <158611991+alexstu1@users.noreply.github.com>
2024-04-10 11:58:17 -0700

    Add files via upload (#2)
    * Add files via upload
    
    * Delete calculator.py
    
    * Delete calculations.py
    
    * rename calc_func.py to calculator.py
    
    * Update and rename calc_notree.py to calculations.py

--- calculations.py
+++ calculations.py
@@ -1,127 +1,90 @@
-class ExpTree:
-    '''
-    builds a binary tree, then post order solves it
-    '''
-    def __init__(self,string):
-        
-        self.order=['^','**','*','/','-','+']
-        self.legal=['^','**','*','/','-','+']
-        self.answer=self.solve(string)
-    def cleanup(self,string):
-        idx=0
-        string=string.strip(' ')
-        while idx<len(string):
-            if string[idx]==' ':
-                string=string[0:idx]+string[idx+1:]
-            idx+=1
-        if string[0]=='-':
-            string='0'+string
-        idx=1
-        while idx<len(string):
-            if string[idx]=='-' and string[idx-1]=='-':
-                string=string[0:idx-1]+'+'+string[idx+1:]
+def _enlist(string):
+    lst=[]
+    hold=''
+    index=-1
+    legal=['*','/','-','+']
+    for chr in string:
+        index+=1
+        if chr==' ':
+            continue
+        if chr.isnumeric() or chr=='.':
+            hold=hold+chr
+            continue
+        elif hold!='':
+            lst.append(float(hold))
+            hold=''
+        if chr in legal:
+            lst.append(chr)
+            continue
+        raise ValueError(f'unimplemented symbol found: {chr}')
+    lst.append(float(hold))
+    return lst
+def _doubles(lst):
+    idx=1
+    while idx<len(lst):
+        if lst[idx]=='-'and lst[idx-1]=='-':
+            lst.pop(idx)
+            lst[idx-1]='+'
+        idx+=1
+    idx=1
+    while idx<len(lst):
+        if lst[idx]=='+'and lst[idx-1]=='+':
+            lst.pop(idx)
+            idx-=1
+        idx+=1
+    return lst
+def _fix_mdas(lst):
+    lst=lst.copy()
+    idx=0
+    while idx<len(lst):
+        if lst[idx]=='-':
+            if idx!=0 and type(lst[idx-1])==float:
+                lst[idx]='+'
+            else:
+                lst.pop(idx)
                 idx-=1
-            idx+=1
-        idx=1
-        while idx<len(string):
-            if string[idx]=='+' and string[idx-1]=='+':
-                string=string[0:idx]+string[idx+1:]
-                idx-=1
-            idx+=1
-        return string
-    def enlist(self,string):
-        lst=[]
-        hold=''
-        index=-1
-        for chr in string:
-            index+=1
-            #duplicate space protection here, refactor this
-            if chr==' ':
-                continue
-            if chr.isnumeric() or chr=='.':
-                hold=hold+chr
-                continue
-            elif hold!='':
-                lst.append(float(hold))
-                hold=''
-            if chr in self.legal:
-                lst.append(chr)
-                continue
-            raise ValueError(f'unimplemented symbol found: {chr}')
-        lst.append(float(hold))
-        return lst
-    def fix_neg_div(self,lst):
+            lst[idx+1]=0-lst[idx+1]
+        idx+=1
+    idx=0
+    while idx<len(lst):
+        if lst[idx]=='/':
+            lst[idx]='*'
+            lst[idx+1]=1/lst[idx+1]
+        idx+=1
+    return lst
+    
+def _parse(lst):
+    lst=lst.copy()
+    order=['*','+']
+    for sym in order:
         idx=0
         while idx<len(lst):
-            if lst[idx]=='-':
-                if type(lst[idx-1])==float:
-                    lst[idx]='+'
-                else:
-                    lst.pop(idx)
-                    idx-=1
-                lst[idx+1]=0-lst[idx+1]
+            if len(lst)==1: 
+                return lst[0]
+            if sym==lst[idx]:
+                left=lst.pop(idx-1)
+                right=lst.pop(idx)
+                lst[idx-1]=_math(left,sym,right)
+                idx=-1
             idx+=1
-        idx=0
-        while idx<len(lst):
-            if lst[idx]=='/':
-                lst[idx]='*'
-                lst[idx+1]=1/lst[idx+1]
-            idx+=1
-        return lst
-    def parse(self,lst):
-        for sym in self.order:
-            #print(sym,lst)
-            idx=0
-            while idx<len(lst):
-                #print(lst)
-                if len(lst)==1: 
-                    if type(lst[0])==Node:
-                        held=lst[0]
-                        return held.solve()
-                    return lst[0]
-                if sym==lst[idx]:
-                    #print(sym,lst)
-                    node1=Node(lst.pop(idx))
-                    if type(lst[idx-1])==Node:
-                        node1.left=lst.pop(idx-1)
-                    else:
-                        node1.left=Node(lst.pop(idx-1))
-                    if type(lst[idx-1])==Node:
-                        node1.right=lst.pop(idx-1)
-                    else:
-                        node1.right=Node(lst.pop(idx-1))
-                    lst.insert(idx-1,node1)
-                    idx=-1
-                idx+=1
-    def solve(self,string):
-        string=self.cleanup(string)
-        lst=self.enlist(string)
-        lst=self.fix_neg_div(lst)
-        return self.parse(lst)
-#[parenthesis,exponent,mult,div,add,subt]
+    raise RuntimeError('Unexpected error during math operaitons')
+def _math(left,sym,right):
+    pointers={'*':_mult,'+':_add}
+    for key in pointers:
+        if sym==key:
+            return pointers[key](left,right)
+def _add(a,b):
+    return a+b
+def _mult(a,b):
+    return a*b
+def solve(string):
+    lst=_enlist(string)
+    lst=_doubles(lst)
+    lst=_fix_mdas(lst)
+    return _parse(lst)
 
 
-class Node:
-    def __init__(self,value):
-        self.value=value
-        self.left=None
-        self.right=None
-        self.operators={'+':self.add,'-':self.minus,'*':self.mult,'/':self.div,'**':self.exp,'^':self.exp}
-
-    def add(self,a,b):
-        return a+b
-    def minus(self,a,b):
-        return a-b
-    def mult(self,a,b):
-        return a*b
-    def div(self,a,b):
-        return a/b
-    def exp(a,b):
-        return a**b
-    def solve(self):
-        if self.left==None and self.right==None:
-            return self.value
-        left=self.left.solve()
-        right=self.right.solve()
-        answer=self.operators[self.value](left,right)
-        return answer
+def _test():
+    print(solve('1+2-3*4/3'))
+if __name__=='__main__':
+    _test()
--- calculator.py
+++ calculator.py
@@ -1,216 +1,231 @@
 from tkinter import *
-from calculations import ExpTree
-class Window:
-    def __init__(self,width):
-        self.root=Tk()
-        self.root.title('Calculator')
-        self.ent=Entry(self.root,width=width,borderwidth=5,justify=RIGHT)
-        self.ent.grid(row=0,column=1,columnspan=4)
-        self.ent.focus_set()
-        self.running=False
-        self._init_labelFrames()
-        self._init_number_buttons()
-        self._init_basic_math_buttons()
-        self._init_show_eq(width)
-        self._init_memory()
-        self._init_history()
-        self.clear_all()
-    def _init_history(self):
-        self.hist_button=Button(self.history_frame,text='History',padx=30,pady=10,command=self.swap_history)
-        self.mem_button=Button(self.history_frame,text='Memory',padx=30,pady=10,command=self.swap_memory)
-        self.hist_button.grid(row=0,column=0)
-        self.mem_button.grid(row=0,column=1)
-        self.no_mem=Label(self.history_frame,text='No numbers in memory!')
-        self.no_hist=Label(self.history_frame,text='No history yet!')
-        self.sub_frame_hist=LabelFrame(self.history_frame,borderwidth=5)
-        self.sub_frame_hist.grid(row=1,column=0,columnspan=2,sticky=NS)
-        self._is_hist=False
-        self.hist_list=[]
-        self.swap_history()
-    def swap_history(self):
-        self._is_hist=True
-        self.update_mem_or_hist()
-    def swap_memory(self):
-        self._is_hist=False
-        self.update_mem_or_hist()
-    def update_mem_or_hist(self):
-        self.sub_frame_hist.destroy()
-        self.sub_frame_hist=LabelFrame(self.history_frame,borderwidth=0)
-        self.sub_frame_hist.grid(row=1,column=0,columnspan=2,sticky=NS)
-        self.no_hist.grid_forget()
-        self.no_mem.grid_forget()
-        if self._is_hist and len(self.hist_list)!=0:
-            idx=0
-            while idx<len(self.hist_list):
-                lab=Label(self.sub_frame_hist,text=self.hist_list[idx])
-                lab.grid(row=1+idx,column=0,columnspan=2)
-                idx+=1
-        elif self._is_hist:
-            self.no_hist.grid(row=1,column=0,columnspan=2)
-        elif not self._is_hist and self.mem_hist==[0]:
-            self.no_mem.grid(row=1,column=0,columnspan=2)
-        else:
-            idx=0
-            while idx<len(self.mem_hist):
-                lab=Button(self.sub_frame_hist,text=self.mem_hist[idx],padx=10,pady=5,command=lambda index=idx: self.set_screen(self.mem_hist[index]))
-                lab.grid(row=1+idx,column=0,columnspan=2)
-                idx+=1
-    def _init_labelFrames(self):
-        self.basic_frame=LabelFrame(self.root,padx=5,pady=5)
-        self.basic_frame.grid(row=3,column=1)
-        self.special_frame=LabelFrame(self.root,padx=5,pady=5)
-        self.special_frame.grid(row=3,column=2,sticky=S)
-        self.memory_frame=LabelFrame(self.root,padx=5,pady=5)
-        self.memory_frame.grid(row=2,column=1,sticky=E)
-        self.history_frame=LabelFrame(self.root,padx=5,pady=5)
-        self.history_frame.grid(row=0,column=0,rowspan=10,sticky=NS)
-    def _init_number_buttons(self):
-        self.button1=Button(self.basic_frame,text='1',padx=40,pady=20,command=lambda: self.button_click(1))
-        self.button2=Button(self.basic_frame,text='2',padx=40,pady=20,command=lambda: self.button_click(2))
-        self.button3=Button(self.basic_frame,text='3',padx=40,pady=20,command=lambda: self.button_click(3))
-        self.button4=Button(self.basic_frame,text='4',padx=40,pady=20,command=lambda: self.button_click(4))
-        self.button5=Button(self.basic_frame,text='5',padx=40,pady=20,command=lambda: self.button_click(5))
-        self.button6=Button(self.basic_frame,text='6',padx=40,pady=20,command=lambda: self.button_click(6))
-        self.button7=Button(self.basic_frame,text='7',padx=40,pady=20,command=lambda: self.button_click(7))
-        self.button8=Button(self.basic_frame,text='8',padx=40,pady=20,command=lambda: self.button_click(8))
-        self.button9=Button(self.basic_frame,text='9',padx=40,pady=20,command=lambda: self.button_click(9))
-        self.button0=Button(self.basic_frame,text='0',padx=40,pady=20, command=lambda: self.button_click(0))
-        self._display_num_buttons()
-    def _display_num_buttons(self):
-        self.button1.grid(row=4,column=0)
-        self.button2.grid(row=4,column=1)
-        self.button3.grid(row=4,column=2)
-        self.button4.grid(row=3,column=0)
-        self.button5.grid(row=3,column=1)
-        self.button6.grid(row=3,column=2)
-        self.button7.grid(row=2,column=0)
-        self.button8.grid(row=2,column=1)
-        self.button9.grid(row=2,column=2)
-        self.button0.grid(row=5,column=1)
-    def _init_basic_math_buttons(self):
-        self.button_add=Button(self.basic_frame,text='+',padx=39,pady=20,command=lambda: self.hold('+'))
-        self.button_subt=Button(self.basic_frame,text='-',padx=40,pady=20,command=lambda: self.hold('-'))
-        self.button_mult=Button(self.basic_frame,text='*',padx=40,pady=20,command=lambda: self.hold('*'))
-        self.button_div=Button(self.basic_frame,text='/',padx=40,pady=20,command=lambda: self.hold('/'))
-        self.button_neg=Button(self.basic_frame,text='+/-',padx=35,pady=20,command=self.negative_swap)
-        self.button_dec=Button(self.basic_frame,text='.',padx=42,pady=20,command=lambda: self.button_click('.'))
-        self.button_bksp=Button(self.special_frame,text='Bksp',padx=52,pady=20,command= self.bksp)
-        self.button_equals=Button(self.special_frame,text='=',padx=60,pady=20,command=self.equals)
-        self.button_clear_all=Button(self.special_frame,text='C',padx=60,pady=20,command=self.clear_all)
-        self.button_clear_entry=Button(self.special_frame,text='CE',padx=57,pady=20,command=self.clear_entry)
-        self._display_basic_math_buttons()
-    def _display_basic_math_buttons(self):
-       self.button_add.grid(row=2,column=3)
-       self.button_subt.grid(row=3,column=3)
-       self.button_mult.grid(row=4,column=3)
-       self.button_div.grid(row=5,column=3) 
-       self.button_neg.grid(row=5,column=0)
-       self.button_dec.grid(row=5,column=2)
-       self.button_bksp.grid(row=0,column=0)
-       self.button_equals.grid(row=3,column=0)
-       self.button_clear_all.grid(row=2,column=0) 
-       self.button_clear_entry.grid(row=1,column=0) 
-    def _init_show_eq(self,width):
-        self.show_eq_text= StringVar()
-        self.show_eq=Label(self.root,width=width,textvariable=self.show_eq_text)
-        self.show_eq.grid(row=1,column=1,columnspan=4)
-    def _init_memory(self):
-        self.mem_hist=[0]
-        self.button_mc=Button(self.memory_frame,text='MC',padx=20,pady=10,command=self.mem_clear)
-        self.button_mr=Button(self.memory_frame,text='MR',padx=20,pady=10,command=lambda: self.set_screen(self.mem_hist[0]))
-        self.button_mem_add=Button(self.memory_frame,text='M+',padx=20,pady=10,command=lambda:self.mem_add(float(self.ent.get())))
-        self.button_mem_subt=Button(self.memory_frame,text='M-',padx=20,pady=10,command=lambda:self.mem_subt(float(self.ent.get())))
-        self.button_ms=Button(self.memory_frame,text='MS',padx=20,pady=10,command=lambda: self.mem_store(float(self.ent.get())))
-        self._display_memory()
-    def _display_memory(self):
-        self.button_mc.grid(row=0,column=0)
-        self.button_mr.grid(row=0,column=1)
-        self.button_mem_add.grid(row=0,column=2)
-        self.button_mem_subt.grid(row=0,column=3)
-        self.button_ms.grid(row=0,column=4)
-    def negative_swap(self):
-        current=self.ent.get()
-        if current.lstrip('-')!=current:
-            self.ent.delete(0)
-        else:
-            self.ent.insert(0,'-')
-    def bksp(self):
-        if len(self.ent.get())>0:
-            self.ent.delete(len(self.ent.get())-1)
-    def set_screen(self,val):
-        self.ent.delete(0,END)
-        self.ent.insert(0,val)
-    def button_click(self,button):
-        current=self.ent.get()
-        self.ent.delete(0,END)
-        self.ent.insert(0,str(current)+str(button))
-    def clear_entry(self):
-        if len(self.ent.get())==0:
-            self.clear_all()
-        self.ent.delete(0,END)
-    def clear_all(self):
-        self.ent.delete(0,END)
-        self.show_eq_text.set('')
-        self.held=''
-        self.held_sym=''
-        self.hist_list=[]
-        self.update_mem_or_hist()
-    def hold(self,button):
-        if self.held == '':
-            self.held=str(self.ent.get())
-        if self.held!='0':
-            self.held=self.held.lstrip('0 ')
-        self.held_sym=button
-        self.ent.delete(0,END)
-        self.show_eq_text.set(self.held+' '+self.held_sym)
-    def equals(self):
-        '''
-        from the eval() era
-        if self.held_sym==None or self.ent.get()=='' or self.held==None:
-            return
-        '''
-        if self.ent.get()=='0' and self.held_sym=='/':
-            self.clear_entry()
-            self.ent.insert(0,'ERROR: Divide by 0')
-            return
-        hold=str(self.ent.get())
-        hold=hold.lstrip(' 0')
-
-        tree=ExpTree(self.held+self.held_sym+hold)
-        result=tree.answer
-        self.hist_list.append(self.held+self.held_sym+hold+'='+str(result))
-        if len(self.hist_list)>15:
-            self.hist_list.pop(0)
-        self.update_mem_or_hist()
-        self.ent.delete(0,END)
-        self.show_eq_text.set('')
-        self.held=''
-        self.held_sym=''
-        self.ent.insert(0,result)
-    def mem_clear(self):
-        self.mem_hist=[0]
-        self.update_mem_or_hist()
-    def mem_add(self,val):
-        self.mem_hist[0]=self.mem_hist[0]+float(val)
-        self.update_mem_or_hist()
-    def mem_subt(self,val):
-        self.mem_hist[0]=self.mem_hist[0]-float(val)
-        self.update_mem_or_hist()
-    def mem_store(self,val):
-        if self.mem_hist[0]==0:
-            self.mem_hist[0]=val
-        else:
-            self.mem_hist.insert(0,val)
-        if len(self.mem_hist)>9:
-            self.mem_hist.pop(9)
-        self.update_mem_or_hist()
-    def math_cleanup(self):
-        #when making more buttons that do a calc, make this funcion as a cleaner
-        pass
+from calc_notree import solve
+def startup():
+    root=Tk()
+    root.title('Calculator')
+    ent=Entry(root,width=50,borderwidth=5,justify=RIGHT)
+    ent.grid(row=0,column=1,columnspan=4)
+    ent.focus_set()
+    ent.clipboard_append('h')
+    memlst=[]
+    histlist=[]
+    frames=labelFrames(root)
+    numberButtons(frames[0],ent)
+    history(frames[3],histlist,memlst,ent)
+    basicMath(frames[0],frames[1],ent,histlist,memlst)
+    memory(frames[2],memlst,ent)
+    return root
 
 
-        
+def labelFrames(root):
+    basic_frame=LabelFrame(root,padx=5,pady=5)
+    basic_frame.grid(row=3,column=1)
+    special_frame=LabelFrame(root,padx=5,pady=5)
+    special_frame.grid(row=3,column=2,sticky=S)
+    memory_frame=LabelFrame(root,padx=5,pady=5)
+    memory_frame.grid(row=2,column=1,sticky=E)
+    history_frame=LabelFrame(root,padx=5,pady=5)
+    history_frame.grid(row=0,column=0,rowspan=10,padx=5,pady=5,sticky=NS)
+    return [basic_frame,special_frame,memory_frame,history_frame]
+
+def numberButtons(frame,ent):
+    button1=Button(frame,text='1',padx=40,pady=20,command=lambda: button_click('1',ent))
+    button2=Button(frame,text='2',padx=40,pady=20,command=lambda: button_click('2',ent))
+    button3=Button(frame,text='3',padx=40,pady=20,command=lambda: button_click('3',ent))
+    button4=Button(frame,text='4',padx=40,pady=20,command=lambda: button_click('4',ent))
+    button5=Button(frame,text='5',padx=40,pady=20,command=lambda: button_click('5',ent))
+    button6=Button(frame,text='6',padx=40,pady=20,command=lambda: button_click('6',ent))
+    button7=Button(frame,text='7',padx=40,pady=20,command=lambda: button_click('7',ent))
+    button8=Button(frame,text='8',padx=40,pady=20,command=lambda: button_click('8',ent))
+    button9=Button(frame,text='9',padx=40,pady=20,command=lambda: button_click('9',ent))
+    button0=Button(frame,text='0',padx=40,pady=20, command=lambda: button_click('0',ent))
+
+    button1.grid(row=4,column=0)
+    button2.grid(row=4,column=1)
+    button3.grid(row=4,column=2)
+    button4.grid(row=3,column=0)
+    button5.grid(row=3,column=1)
+    button6.grid(row=3,column=2)
+    button7.grid(row=2,column=0)
+    button8.grid(row=2,column=1)
+    button9.grid(row=2,column=2)
+    button0.grid(row=5,column=1)
+def button_click(button,ent):
+    current=ent.get()
+    ent.delete(0,END)
+    ent.insert(0,current+button)
+def basicMath(frame,sframe,ent,histlist,memlst):
+    button_add=Button(frame,text='+',padx=39,pady=20,command=lambda: button_click('+',ent))
+    button_subt=Button(frame,text='-',padx=40,pady=20,command=lambda: button_click('-',ent))
+    button_mult=Button(frame,text='*',padx=40,pady=20,command=lambda: button_click('*',ent))
+    button_div=Button(frame,text='/',padx=40,pady=20,command=lambda: button_click('/',ent))
+    button_neg=Button(frame,text='+/-',padx=35,pady=20,command=lambda: negative_swap(ent))
+    button_dec=Button(frame,text='.',padx=42,pady=20,command=lambda: button_click('.',ent))
+    button_bksp=Button(sframe,text='Bksp',padx=52,pady=20,command= lambda: ent.delete(len(ent.get())-1))
+    button_equals=Button(sframe,text='=',padx=60,pady=20,command=lambda: equals(ent,histlist))
+    button_clear_all=Button(sframe,text='C',padx=60,pady=20,command=lambda: clear_all(ent,histlist,memlst))
+    button_clear_entry=Button(sframe,text='CE',padx=57,pady=20,command=lambda: clear_entry(ent,histlist,memlst))
+
+    button_add.grid(row=2,column=3)
+    button_subt.grid(row=3,column=3)
+    button_mult.grid(row=4,column=3)
+    button_div.grid(row=5,column=3) 
+    button_neg.grid(row=5,column=0)
+    button_dec.grid(row=5,column=2)
+    button_bksp.grid(row=0,column=0)
+    button_equals.grid(row=3,column=0)
+    button_clear_all.grid(row=2,column=0) 
+    button_clear_entry.grid(row=1,column=0) 
+
+def negative_swap(ent):
+    current=ent.get()
+    if current=='':
+        ent.insert(0,'-')
+        return
+    idx=len(current)-1
+    while idx>=0:
+        if current[idx]=='-':
+            if idx==0:
+                ent.delete(0)
+                break
+            elif current[idx-1] not in '1234567890.':
+                ent.delete(0,END)
+                string=current[0:idx]+current[idx+1:]
+                ent.insert(0,string)
+                break
+            else:
+                ent.delete(0,END)
+                string=current[0:idx]+'+'+current[idx+1:]
+                ent.insert(0,string)
+                break
+        elif current[idx]=='+':
+            ent.delete(0,END)
+            string=current[0:idx]+'-'+current[idx+1:]
+            ent.insert(0,string)
+            break
+        elif current[idx]==')':
+            ent.delete(0,END)
+            string=current[0:idx+1]+'*-'+current[idx+1:]
+            ent.insert(0,string)
+            break
+        elif current[idx] in '*/^':
+            ent.delete(0,END)
+            string=current[0:idx+1]+'-'+current[idx+1:]
+            ent.insert(0,string)
+            break
+        elif current[idx] in '1234567890.':
+            idx-=1
+            continue
+        if idx==0:
+            ent.delete(0,END)
+            ent.insert(0,'-'+current)
+            break
+        raise SyntaxError(f'Unexpected symbol found: {current[idx]}')
+def equals(ent,histlist):
+    answer=solve(ent.get())
+    histlist.insert(0,ent.get()+'='+str(answer))
+    ent.delete(0,END)
+    ent.insert(0,answer)
+    update()
+def clear_all(ent,histlist,memlst):
+    while histlist!=[]:
+        histlist.pop()
+    while memlst!=[]:
+        memlst.pop()
+    ent.delete(0,END)
+    update()
+def clear_entry(ent,histlst,memlst):
+    if len(ent.get())==0:
+        clear_all(ent,histlst,memlst)
+    ent.delete(0,END)
+    update()
+def memory(frame,lst,ent):
+    button_mc=Button(frame,text='MC',padx=20,pady=10,command=lambda: memhelp(ent,memclear(lst)))
+    button_mr=Button(frame,text='MR',padx=20,pady=10,command=lambda: memhelp(ent,memrecall(ent,lst)))
+    button_mem_add=Button(frame,text='M+',padx=20,pady=10,command=lambda: memhelp(ent,memadd(solve(ent.get()),lst)))
+    button_mem_subt=Button(frame,text='M-',padx=20,pady=10,command=lambda: memhelp(ent,memsub(solve(ent.get()),lst)))
+    button_ms=Button(frame,text='MS',padx=20,pady=10,command=lambda: memhelp(ent,mem_store(solve(ent.get()),lst)))
+    button_mc.grid(row=0,column=0)
+    button_mr.grid(row=0,column=1)
+    button_mem_add.grid(row=0,column=2)
+    button_mem_subt.grid(row=0,column=3)
+    button_ms.grid(row=0,column=4)
+def memhelp(ent,func):
+    #the func gets called in the functions calling this so both can be done in a lambda function
+    if ent.clipboard_get()=='m':
+        update()
+def memrecall(ent,lst):
+    ent.delete(0,END)
+    if lst!=[]:
+        ent.insert(0,lst[0])
+def mem_store(num,lst):
+        #!reminder
+        lst.insert(0,num)
+def memclear(lst):
+    while lst!=[]:
+        lst.pop()
+def memadd(num,lst):
+    if lst==[]:
+        lst.append(num)
+    else:
+        lst[0]=lst[0]+num
+def memsub(num,lst):
+    if lst==[]:
+        lst.append(0-num)
+    else:
+        lst[0]=lst[0]-num
+def history(frame,histlist,memlist,ent):
+    subframe=LabelFrame(frame,borderwidth=5)
+    hist_button=Button(frame,text='History',padx=30,pady=10,command=lambda: swapper('hist',ent))
+    mem_button=Button(frame,text='Memory',padx=30,pady=10,command=lambda: swapper('mem',ent))  
+    hist_button.grid(row=0,column=0)
+    mem_button.grid(row=0,column=1)
+    subframe.grid(row=1,column=0,columnspan=2,sticky=NS)
+    update(ent,subframe,histlist,memlist)
+def swapper(target,ent):
+    if target=='hist':
+        ent.clipboard_clear()
+        ent.clipboard_append('h')
+    elif target=='mem':
+        ent.clipboard_clear()
+        ent.clipboard_append('m')
+    update()        
+def update(ent=None,subframe=None,histpoint=None,mempoint=None,pointers=[]):
+    if histpoint!=None:
+        pointers.append(histpoint)
+        pointers.append(mempoint)
+        pointers.append(subframe)
+        pointers.append(ent)
+    ent=pointers[3]
+    subframe=pointers[2]
+    memlst=pointers[1]
+    histlist=pointers[0]
+    target=ent.clipboard_get()
+    hold=[]
+    for item in subframe.children.values():
+        hold.append(item)
+    while hold!=[]:
+        hold.pop().destroy()
+    no_hist=Label(subframe,text='No history yet!')
+    no_mem=Label(subframe,text='No numbers in memory!')
+    if target=='h' and len(histlist)!=0:
+        idx=0
+        while idx<len(histlist):
+            lab=Label(subframe,text=histlist[idx])
+            lab.grid(row=1+idx,column=0,columnspan=2)
+            idx+=1
+    elif target=='h':
+        no_hist.grid(row=1,column=0,columnspan=2)
+    elif target=='m' and memlst!=[]:
+        idx=0
+        while idx<len(memlst):
+            hold=memlst[idx]
+            lab=Button(subframe,text=memlst[idx],padx=10,pady=5,command=lambda held=hold: ent.insert(len(ent.get()),held))
+            lab.grid(row=1+idx,sticky=EW,column=0,columnspan=2)
+            idx+=1
+    elif target=='m':
+        no_mem.grid(row=1,column=0,columnspan=2)
 def main():
-    win=Window(50)
-    win.root.mainloop()
+    root=startup()
+    root.mainloop()
 main()
